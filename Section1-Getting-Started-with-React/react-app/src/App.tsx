8b2ec9be2c4e8a69ef2d2f67da42c3b812575bf0
bikramia <bikram.rhce@gmail.com>
2024-10-04 14:11:39 -0400

    Passing Functions via Props

--- Section1-Getting-Started-with-React/react-app/src/App.tsx
+++ Section1-Getting-Started-with-React/react-app/src/App.tsx
@@ -3,9 +3,13 @@ import ListGroup from "./components/ListGroup";
 // PascalCasing
 function App() {
   let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
+
+  const handleSelecItem = (item: string) => {
+    console.log(item);
+  }
   return (
     <div>
-      <ListGroup items={items} heading="cities"/>
+      <ListGroup items={items} heading="cities" onSelectionItem={handleSelecItem}/>
     </div>
   );
 }
--- Section1-Getting-Started-with-React/react-app/src/components/ListGroup.tsx
+++ Section1-Getting-Started-with-React/react-app/src/components/ListGroup.tsx
@@ -3,31 +3,34 @@ import { useState } from "react";
 interface Props {
   items: string[];
   heading: string;
+  onSelectionItem: (item: string) => void;
 }
 
-function ListGroup({ items, heading }: Props) {
+function ListGroup({ items, heading, onSelectionItem }: Props) {
   // Hook-A hook is a function that allow us to tap into built-in features in React
-  const [selectionIndex, setSelectedIndex] = useState(-1)
+  const [selectionIndex, setSelectedIndex] = useState(-1);
 
-  
   return (
     <>
       <h1>{heading}</h1>
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
-        {items.map(
-          (
-            item, index
-          ) => (
-            <li
-              className={ selectionIndex === index ? 'list-group-item active' : 'list-group-item'}
-              key={item}
-              onClick={() => { setSelectedIndex(index) }} 
-            >
-              {item}
-            </li>
-          )
-        )}
+        {items.map((item, index) => (
+          <li
+            className={
+              selectionIndex === index
+                ? "list-group-item active"
+                : "list-group-item"
+            }
+            key={item}
+            onClick={() => {
+              setSelectedIndex(index);
+              onSelectionItem(item)
+            }}
+          >
+            {item}
+          </li>
+        ))}
       </ul>
     </>
   );
