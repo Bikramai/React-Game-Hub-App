9cc9e5eaee31f40a65e9d522593ce914b4d46471
bikramia <bikram.rhce@gmail.com>
2024-10-24 07:46:33 -0400

    Need to fix - got the button but not showing styles css color

--- Section3-Styling-Components/react-app/src/App.tsx
+++ Section3-Styling-Components/react-app/src/App.tsx
@@ -1,20 +1,9 @@
-// import { useState } from "react";
-// import Alert from "./components/Alert";
-// import Button from "./components/Button";
-// import './App.css'
-// import ListGroup from "./components/ListGroup";
 
-import { IoCalendarNumber } from "react-icons/io5";
 
 function App() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"];
-
-  const handleSelecItem = (item: string) => {
-    console.log(item);
-  }
   return (
     <div>
-      <IoCalendarNumber color="red" size="80"/>
+      <button onClick={() => {}}> My Button</button>
     </div>
   );
 }
