c7e7e9fd69526facdec005a4f828592fc1e4e2ae
bikramia <bikram.rhce@gmail.com>
2024-10-27 11:36:53 -0400

    Building a like component - 2nd step rendering empty heart

--- Section3-Styling-Components/react-app/src/components/Like.tsx
+++ Section3-Styling-Components/react-app/src/components/Like.tsx
@@ -1,7 +1,11 @@
-import { IoIosHeart } from "react-icons/io";
+import { useState } from "react";
+import { IoIosHeart, IoIosHeartEmpty } from "react-icons/io";
 
 const Like = () => {
-  return <IoIosHeart color="#ff6b81" size={40} />;
+  const [status, setStatus] = useState(false);
+
+  if (status) return <IoIosHeart color="#ff6b81" size={40} />;
+  return <IoIosHeartEmpty size={40} />;
 };
 
 export default Like;
