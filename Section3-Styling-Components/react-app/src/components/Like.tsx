128fbf43dfc2ba8ab1e46ca5ca0690c39f8747e9
bikramia <bikram.rhce@gmail.com>
2024-10-28 07:31:52 -0400

    Building a Like Component -step3 Handling onClick events

--- Section3-Styling-Components/react-app/src/components/Like.tsx
+++ Section3-Styling-Components/react-app/src/components/Like.tsx
@@ -4,8 +4,11 @@ import { IoIosHeart, IoIosHeartEmpty } from "react-icons/io";
 const Like = () => {
   const [status, setStatus] = useState(false);
 
-  if (status) return <IoIosHeart color="#ff6b81" size={40} />;
-  return <IoIosHeartEmpty size={40} />;
+  if (status)
+    return (
+      <IoIosHeart color="#ff6b81" size={40} onClick={() => setStatus(false)} />
+    );
+  return <IoIosHeartEmpty size={40} onClick={() => setStatus(true)} />;
 };
 
 export default Like;
