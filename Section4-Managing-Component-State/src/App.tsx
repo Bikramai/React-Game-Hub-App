ec4fae446a20c806029841f0682cac42134ea22f
bikramia <bikram.rhce@gmail.com>
2024-11-10 08:12:27 -0500

    Project-ExpandableText Components

--- Section4-Managing-Component-State/src/App.tsx
+++ Section4-Managing-Component-State/src/App.tsx
@@ -1,34 +1,9 @@
-import { useState } from "react";
-import ListGroup from "./components/ListGroup";
+import ExpandableText from "./components/ExpandableText";
 
 function App() {
-  const [cart, setCart] = useState({
-    discount: 0.1,
-    items: [
-      { id: 1, title: "Product 1", quantity: 1 },
-      { id: 2, title: "Product 2", quantity: 1 },
-    ],
-  });
-
-  const handleClick = () => {
-    setCart({
-      ...cart,
-      items: cart.items.map((item) =>
-        item.id === 1 ? { ...item, quantity: item.quantity + 1 } : item
-      ),
-    });
-  };
-
-  const [game, setGame] = useState({
-    id: 1,
-    player: {
-      name: "Bikram",
-    },
-  });
-
   return (
     <div>
-      <button onClick={handleClick}>Click Me</button>
+      <ExpandableText></ExpandableText>
     </div>
   );
 }
