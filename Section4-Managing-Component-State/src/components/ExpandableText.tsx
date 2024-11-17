6e850094bc8204a1215297c4b3b4990ad68bed75
bikramia <bikram.rhce@gmail.com>
2024-11-16 21:16:37 -0500

    Project completed Building ExpandableText Component

--- Section4-Managing-Component-State/src/components/ExpandableText.tsx
+++ Section4-Managing-Component-State/src/components/ExpandableText.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
 interface Props {
   children: string;
+  maxChars?: number;
 }
 
-const ExpandableText = ({ children }: Props) => {
-  return <div>{children}</div>;
+const ExpandableText = ({ children, maxChars = 100 }: Props) => {
+  const [isExpanded, setExpanded] = useState(false);
+
+  if (children.length <= maxChars) return <p>{children}</p>;
+
+  const text = isExpanded ? children : children.substring(0, maxChars);
+
+  return (
+    <p>
+      {text}...
+      <button onClick={() => setExpanded(!isExpanded)}>
+        {isExpanded ? "Less" : "More"}
+      </button>
+    </p>
+  );
 };
 
 export default ExpandableText;
