9babc59f1785f3c295f5f08c7e5bf9e4535743cb
bikramia <bikram.rhce@gmail.com>
2024-12-14 11:15:37 -0500

    Project-ExpenseList Tracker-built delete button and table disappear function

--- Section5-Building-Forms/src/App.tsx
+++ Section5-Building-Forms/src/App.tsx
@@ -1,18 +1,19 @@
+import { useState } from "react";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 function App() {
-  const expenses = [
+  const [expenses, setExpenses] = useState([
     { id: 1, description: "Car", amount: 500, category: "CarLoan" },
     { id: 2, description: "House", amount: 2000, category: "HouseLoan" },
     { id: 3, description: "Grocery", amount: 800, category: "MonthlyGrocery" },
     { id: 4, description: "Gas", amount: 300, category: "CarGas" },
-  ];
+  ]);
 
   return (
     <div>
       <ExpenseList
         expenses={expenses}
-        onDelete={(id) => console.log("Delete", id)}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
       />
     </div>
   );
--- Section5-Building-Forms/src/expense-tracker/components/ExpenseList.tsx
+++ Section5-Building-Forms/src/expense-tracker/components/ExpenseList.tsx
@@ -13,6 +13,8 @@ interface Props {
 }
 
 const ExpenseList = ({ expenses, onDelete }: Props) => {
+  if (expenses.length === 0) return null; // table disappear function
+
   return (
     <table className="table table-bordered">
       <thead>
