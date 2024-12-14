bd521186d8599921dfcefe56fa2094f70d5de484
bikramia <bikram.rhce@gmail.com>
2024-12-14 11:04:27 -0500

    ## Project-ExpenseList Tracker-Built total list

--- Section5-Building-Forms/src/App.tsx
+++ Section5-Building-Forms/src/App.tsx
@@ -1,9 +1,19 @@
-import Form from "./components/Form";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
 
 function App() {
+  const expenses = [
+    { id: 1, description: "Car", amount: 500, category: "CarLoan" },
+    { id: 2, description: "House", amount: 2000, category: "HouseLoan" },
+    { id: 3, description: "Grocery", amount: 800, category: "MonthlyGrocery" },
+    { id: 4, description: "Gas", amount: 300, category: "CarGas" },
+  ];
+
   return (
     <div>
-      <Form />
+      <ExpenseList
+        expenses={expenses}
+        onDelete={(id) => console.log("Delete", id)}
+      />
     </div>
   );
 }
--- Section5-Building-Forms/src/expense-tracker/components/ExpenseList.tsx
+++ Section5-Building-Forms/src/expense-tracker/components/ExpenseList.tsx
@@ -7,7 +7,12 @@ interface Expense {
   category: string;
 }
 
-const ExpenseList = () => {
+interface Props {
+  expenses: Expense[];
+  onDelete: (id: number) => void;
+}
+
+const ExpenseList = ({ expenses, onDelete }: Props) => {
   return (
     <table className="table table-bordered">
       <thead>
@@ -18,7 +23,36 @@ const ExpenseList = () => {
           <th></th>
         </tr>
       </thead>
-      <tbody></tbody>
+      <tbody>
+        {expenses.map((expense) => (
+          <tr key={expense.id}>
+            <td>{expense.description}</td>
+            <td>{expense.amount}</td>
+            <td>{expense.category}</td>
+            <td>
+              <button
+                className="btn btn-outline-danger"
+                onClick={() => onDelete(expense.id)}
+              >
+                Delete
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+      <tfoot>
+        <tr>
+          <td>Total</td>
+          <td>
+            $
+            {expenses
+              .reduce((acc, expense) => expense.amount + acc, 0)
+              .toFixed(2)}
+          </td>
+          <td></td>
+          <td></td>
+        </tr>
+      </tfoot>
     </table>
   );
 };
