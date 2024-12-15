19e91fd3b362173a06ea17df71d58ef8af58ab83
bikramia <bikram.rhce@gmail.com>
2024-12-15 12:22:09 -0500

    Building Expenses Filter -created category filter with margin of 3

--- Section5-Building-Forms/src/App.tsx
+++ Section5-Building-Forms/src/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import ExpenseList from "./expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
 
 function App() {
   const [expenses, setExpenses] = useState([
@@ -7,10 +8,31 @@ function App() {
     { id: 2, description: "House", amount: 2000, category: "HouseLoan" },
     { id: 3, description: "Grocery", amount: 800, category: "MonthlyGrocery" },
     { id: 4, description: "Gas", amount: 300, category: "CarGas" },
+    {
+      id: 5,
+      description: "Electric & Gas",
+      amount: 300,
+      category: "Utilities",
+    },
+    {
+      id: 6,
+      description: "Water & sewers",
+      amount: 300,
+      category: "Utilities",
+    },
+    {
+      id: 7,
+      description: "Waste Recycle",
+      amount: 60,
+      category: "Waste Management",
+    },
   ]);
 
   return (
     <div>
+      <div className="mb-3">
+        <ExpenseFilter onSelectCategory={(category) => console.log(category)} />
+      </div>
       <ExpenseList
         expenses={expenses}
         onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
