162b8ee52912685370f496824d631447fbc28661
bikramia <bikram.rhce@gmail.com>
2024-12-17 07:53:29 -0500

    building expense form component

---
+++ Section5-Building-Forms/src/expense-tracker/components/ExpenseForm.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ExpenseForm = () => {
+  return (
+    <form>
+        <div className="mb-3"><label htmlFor="" className="form-label"></label><input type="text" className="form-control" /></div>
+    </form>
+  )
+}
+
+export default ExpenseForm
