313fd6b2e4e5545858f88d65f43e71d70470cd85
bikramia <bikram.rhce@gmail.com>
2024-12-31 12:37:48 -0500

    Effect Clean Up

--- Section6-Connecting-to-Backend/src/App.tsx
+++ Section6-Connecting-to-Backend/src/App.tsx
@@ -1,21 +1,17 @@
-import { useState } from "react";
-import ProductList from "./components/ProductList";
+import { useEffect } from "react";
+
+const connect = () => console.log("Connecting...");
+const disconnect = () => console.log("Disconnecting");
 
 function App() {
-  const [category, setCategory] = useState('');
+  useEffect(() => { 
+    connect();
 
+    return () => disconnect();
+  });
 
-  return (
-    <div>
-      <select className="form-select" onChange={(e) => setCategory(e.target.value)}>
-        <option value=""></option>
-        <option value="Clothing">Clothing</option>
-        <option value="Household">Household</option>
-        <option value="Electronics">Electronics</option>
-      </select>
-      <ProductList />
-    </div>
-  );
+  return <div></div>
+   
 }
 
 export default App;
