9a8fb51925a5bac68797387163412f62c2d1a127
bikramia <bikram.rhce@gmail.com>
2025-01-16 17:24:48 -0500

    Updating Data

--- Section6-Connecting-to-Backend/src/App.tsx
+++ Section6-Connecting-to-Backend/src/App.tsx
@@ -50,6 +50,18 @@ function App() {
           });
       };
 
+      const updateUser = (user: User) => {
+        const originalUsers = [...users]
+        const updatedUser = { ...user, name: user.name + '!'};
+        setUsers(users.map(u => u.id === user.id ? updatedUser : u));
+
+        axios.patch('https://jsonplaceholder.typicode.com/xusers/' + user.id, updatedUser)
+          .catch(err => {
+            setError(err.message);
+            setUsers(originalUsers)
+          })
+      }
+
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
@@ -59,7 +71,10 @@ function App() {
         {users.map((user) => (
           <li key={user.id} className="list-group-item d-flex justify-content-between">
             {user.name}
-            <button className="btn btn-outline-danger" onClick={() => deleteUser(user)}>Delete</button>
+            <div>
+              <button className="btn btn-outline-secondary mx-2" onClick={() => updateUser(user)}>Update</button>
+              <button className="btn btn-outline-danger" onClick={() => deleteUser(user)}>Delete</button>
+            </div>
           </li>
         ))}
       </ul>
