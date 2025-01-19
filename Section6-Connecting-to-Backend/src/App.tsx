1d5e811bf703a6023bf67a46370e6cd31909aabb
bikramia <bikram.rhce@gmail.com>
2025-01-19 16:50:33 -0500

    Extracting a User Service

--- Section6-Connecting-to-Backend/src/App.tsx
+++ Section6-Connecting-to-Backend/src/App.tsx
@@ -1,10 +1,6 @@
 import { useEffect, useState } from "react";
-import apiClient, { CanceledError } from "./services/api-client";
-
-interface User {
-  id: number;
-  name: string;
-}
+import { CanceledError } from "./services/api-client";
+import userService, { User } from "./services/user-service";
 
 function App() {
   const [users, setUsers] = useState<User[]>([]);
@@ -12,72 +8,80 @@ function App() {
   const [isLoading, setLoading] = useState(false);
 
   useEffect(() => {
-    const controller = new AbortController();
-
     setLoading(true);
-    apiClient
-      .get<User[]>("/users", {
-        signal: controller.signal,
-      })
+    const { request, cancel } = userService.getAllUsers();
+    request
       .then((res) => {
         setUsers(res.data);
         setLoading(false);
       })
       .catch((err) => setError(err.message));
 
-    return () => controller.abort();
+    return () => cancel();
   }, []);
 
   const deleteUser = (user: User) => {
     const originalUsers = [...users];
     setUsers(users.filter((u) => u.id !== user.id));
 
-    apiClient
-      .delete('users/' + user.id)
-      .catch((err) => {
-        setError(err.message)
-        setUsers(originalUsers)
-      });
+    userService.deleteUser(user.id).catch((err) => {
+      setError(err.message);
+      setUsers(originalUsers);
+    });
   };
 
   const addUser = () => {
     const originalUsers = [...users];
-    const newUsers = { id: 0, name: 'Bikram'};
+    const newUsers = { id: 0, name: "Bikram" };
     setUsers([...users, newUsers]);
-    
-    apiClient
-      .post('/users', newUsers)
+
+    userService
+      .createUser(newUsers)
       .then(({ data: savedUser }) => setUsers([savedUser, ...users]))
-          .catch((err) => {
-            setError(err.message);
-            setUsers(originalUsers);
-          });
-      };
+      .catch((err) => {
+        setError(err.message);
+        setUsers(originalUsers);
+      });
+  };
 
-      const updateUser = (user: User) => {
-        const originalUsers = [...users]
-        const updatedUser = { ...user, name: user.name + '!'};
-        setUsers(users.map(u => u.id === user.id ? updatedUser : u));
+  const updateUser = (user: User) => {
+    const originalUsers = [...users];
+    const updatedUser = { ...user, name: user.name + "!" };
+    setUsers(users.map((u) => (u.id === user.id ? updatedUser : u)));
 
-        apiClient.patch('/users/' + user.id, updatedUser)
-          .catch(err => {
-            setError(err.message);
-            setUsers(originalUsers)
-          })
-      }
+    userService.updateUser(updatedUser).catch((err) => {
+      setError(err.message);
+      setUsers(originalUsers);
+    });
+  };
 
   return (
     <>
       {error && <p className="text-danger">{error}</p>}
       {isLoading && <div className="spinner-border"></div>}
-      <button className="btn-primary mb-3" onClick={addUser}>Add</button>
+      <button className="btn-primary mb-3" onClick={addUser}>
+        Add
+      </button>
       <ul className="list-group">
         {users.map((user) => (
-          <li key={user.id} className="list-group-item d-flex justify-content-between">
+          <li
+            key={user.id}
+            className="list-group-item d-flex justify-content-between"
+          >
             {user.name}
             <div>
-              <button className="btn btn-outline-secondary mx-2" onClick={() => updateUser(user)}>Update</button>
-              <button className="btn btn-outline-danger" onClick={() => deleteUser(user)}>Delete</button>
+              <button
+                className="btn btn-outline-secondary mx-2"
+                onClick={() => updateUser(user)}
+              >
+                Update
+              </button>
+              <button
+                className="btn btn-outline-danger"
+                onClick={() => deleteUser(user)}
+              >
+                Delete
+              </button>
             </div>
           </li>
         ))}
